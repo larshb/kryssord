a0bef9d0f6024fd1e653b6d7c585aa4ee4e71c9a
Lars Henrik Bolstad <lahebo@gmail.com>
2026-03-16 21:24:47 +0100

    Add flask app as wrapper API

---
+++ server/app.py
@@ -0,0 +1,59 @@
+import re
+import urllib.request
+import urllib.parse
+
+
+class KryssordOrg:
+    def __init__(self, url: str = "https://kryssord.org"):
+        self.url = url
+
+    def _download_html(self, url: str):
+        response = urllib.request.urlopen(url)
+        return response.read()
+
+    def _parse_html(
+        self,
+        html: bytes,
+        word_regex=r"<td class=\"word\"><a href=\"(.*?)\">(.*?)<\/a><\/td>",
+    ):
+        text = html.decode("utf-8")
+        for match in re.findall(word_regex, text):
+            yield {"word": match[1], "link": match[0]}
+
+    def search(self, a: str, b: str, endpoint: str = "/search.php"):
+        params = {"a": a, "b": b}
+        url = f"{self.url}{endpoint}" + "?" + urllib.parse.urlencode(params)
+        html = self._download_html(url)
+        return {
+            "url": url,
+            "query": {"a": a, "b": b},
+            "results": list(self._parse_html(html)),
+        }
+
+
+# --- Flask -----------------------------------------------------------------------------------------
+import flask
+
+app = flask.Flask(__name__)
+kryssord = KryssordOrg()
+
+
+@app.route("/")
+def wakeup():
+    return flask.jsonify({"status": "alive"})
+
+
+@app.route("/search")
+def search():
+    a = flask.request.args.get("a")  # required
+    b = flask.request.args.get("b")  # optional
+    if not a:
+        return flask.jsonify({"error": "Search word (a) required"}), 400
+    if not b:
+        b = ""
+    search = kryssord.search(a, b)
+    return flask.jsonify(search)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
