79d9620436926b54b4d28bebbbdfde5463b622d3
Lars Henrik Bolstad <lahebo@gmail.com>
2026-03-16 22:52:58 +0100

    Reformat and fix script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = '//kryssord.herokuapp.com/search/'
+const API = 'https://kryssord.onrender.com/search'
 
 var button = $("button");
 var sok = $("#soksynonymer");
@@ -12,7 +12,7 @@ function addWildcard(wildcard) {
 }
 
 function sokSynonym() {
-	document.activeElement.blur();
+  document.activeElement.blur();
   var top_banner = $("top-banner");
   top_banner.addClass("collapse-height");
   document.getElementById("overlay").style.display = "block";
@@ -26,26 +26,34 @@ function sokSynonym() {
     table.removeChild(table.firstChild);
   }
 
-  path = sokeord
+  // Legacy:
+  // path = sokeord
+  // if (losning) {
+  //   path += "/" + losning
+  // }
+  // var url = API + path;
+  // console.log(url);
+
+  // New API where sokeord is put into key 'a' and the optional losning is put into key 'b'
+  var url = `${API}?a=${sokeord}`;
   if (losning) {
-    path += "/" + losning
+    url += `&b=${losning}`;
   }
-  var url = API + path;
   console.log(url);
 
   $.ajax({
     url: url,
     type: 'GET',
     dataType: 'json',
-    error: function() {
-    	alert("Ukjent feil. Rar formatering av løsningsord?");
-    	document.getElementById("overlay").style.display = "none";
+    error: function () {
+      alert("Ukjent feil. Rar formatering av løsningsord?");
+      document.getElementById("overlay").style.display = "none";
     },
-    success: function(data) {
+    success: function (data) {
       var words = data.results;
-      words.forEach(function(element) {
+      words.forEach(function (element) {
 
-        var txt2 = document.createTextNode(element.length);
+        var txt2 = document.createTextNode(element.word.length);
         var td2 = document.createElement("td");
         td2.appendChild(txt2);
         td2.style.width = "10px";
@@ -53,21 +61,33 @@ function sokSynonym() {
 
         var tr = document.createElement("tr");
         var td = document.createElement("td");
-        var txt = document.createTextNode(element);
+        var txt = document.createTextNode(element.word);
         td.appendChild(txt);
         td.className = "mdl-data-table__cell--non-numeric";
         tr.appendChild(td2);
         tr.appendChild(td);
+
+        /* When the row is clicked, replace 'sokeord' with the word in the row and re-search */
+        tr.onclick = function () {
+          document.getElementById("sokeord").value = element.word;
+          document.getElementById("losning").value = "";
+          sokSynonym();
+        }
+
+        /* Use pointer cursor to indicate that the row is clickable */
+        tr.style.cursor = "pointer";
+
         table.appendChild(tr);
       })
       if (words.length == 0) {
         footer = " Ingen treff :( ";
       } else {
-        footer = `${words.length}/${data.nresults}`;
+        footer = `${words.length}`; /*/${data.nresults}`;*/
+        /* TODO: Re-implement total number of results */
       }
       $('<tr><td></td>' +
         ' <td href="#" class="mdl-data-table__cell--non-numeric"> ' +
-        `  <a class="plain" href="${data.uri}">` +
+        `  <a class="plain" href="${data.url}">` +
         footer +
         '  </a>' +
         ' </td> ' +
@@ -77,60 +97,42 @@ function sokSynonym() {
   });
 }
 
-/* sok.on("click", function() {
-  sokSynonym();
-}) */
-
-$(document).ready(function(){
-	$("#show-dialog").click(function(){
+$(document).ready(function () {
+  $("#show-dialog").click(function () {
     $("#info").fadeIn();
   });
-  $(".close").click(function(){
+  $(".close").click(function () {
     $("#info").fadeOut();
   });
-  
-  // Dummy request to wake up Herokuapp
+
+  // Dummy request to wake up Render service
   $.ajax({
-    url: API + 'n%C3%B8tt/KR%3FS*D',
+    url: API + '?a=n%C3%B8tt&b=KR%3FS*D',
     type: 'GET',
     dataType: 'json'
   });
 })
 
-/* Web app */
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  deferredPrompt = e;
-  showInstallPromotion();
-});
-
-
-
 /* Top overlay illustrative */
-
 const $wildcard = $('<span class="dim">?</span>')
 const tin = $("#losning")[0];
 const tr = $("#tilerow")[0];
 
 function parseTiles() {
-	var txt = tin.value.toUpperCase();
-  
+  var txt = tin.value.toUpperCase();
+
   /* Deferr unnecessary wildcards */
-	txt = txt.replace('**', '*');
+  txt = txt.replace('**', '*');
   tin.value = txt;
-  
+
   $("#tilerow").empty();
   for (const char of txt) {
     td = tr.insertCell();
-    
-    if      (char == '*') td.className = 'indef';
+
+    if (char == '*') td.className = 'indef';
     else if (char == '?') $wildcard.clone().appendTo(td);
-    else                  td.innerHTML = char;
-	}
+    else td.innerHTML = char;
+  }
 }
 
 $("#losning").on('input', parseTiles);
-
-
-
-  
